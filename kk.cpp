b4451d271b74ff2ba7a1eb902e6119f2f8b51fb6
NikBostrom <nbostrom@college.harvard.edu>
2017-04-18 15:22:17 -0400

    prepartitioning 1 and 2 compile

--- kk.cpp
+++ kk.cpp
@@ -217,37 +217,150 @@ signed long long kk(vector<signed long long> x){
 	return final;
 }
 
-int vec_prime(vector<signed long long> x, vector<signed long long> p)
-{
-	vector<int> x_prime (size, 0);
-	int residue = numeric_limits<int>::max();
-	for (int i = 0; i < size; ++i){
-		x_prime[i] = 
+vector<signed long long> vec_prime(vector<signed long long> x, vector<signed long long> p){
+	
+	vector<signed long long> x_prime (size, 0);
+	for (int j = 0; j < size; ++j){
+		x_prime[p[j]] = x_prime[p[j]] + x[j];
+	}
+	return x_prime;
+}
 
-		for (int j = 0; j < s.size(); ++j){
-			s[j] = s_rand(gen);
-			if (s[j] == 0){
-				s[j] = -1;
-			}
+int method_b_1(vector<signed long long> x, int iterations){
+	
+	// Creating initial random solution
+	vector<signed long long> s (size, 0);
+	for (int i = 0; i < size; ++i){
+		s[i] = dis(gen);
+	}
+
+	// Create new sequence x_prime that enforces the prepartitioning from p
+	vector<signed long long> x_prime = vec_prime(x, s);
+	
+	int residue = kk(x_prime);
+
+	for (int i = 0; i < iterations; ++i){
+		vector<signed long long> s_prime(size, 0);
+		for (int i = 0; i < size; ++i){
+			s_prime[i] = dis(gen);
 		}
-		int tmp_residue = 0;
-		for (int j = 0; j < x.size(); ++j){
-			tmp_residue += s[j]*x[j];
-		}
-		tmp_residue = abs(tmp_residue);
-		if (tmp_residue < residue){
-			residue = tmp_residue;
+	
+		int temp_residue = kk(s_prime);
+		if (temp_residue < residue){
+			residue = temp_residue;
 		}
 	}
 	return residue;
 }
 
-int method_b_2(vector<signed long long>, int)
-{
+int method_b_2(vector<signed long long> x, int iterations){
 
+	// Creating initial random solution
+	vector<signed long long> s (size, 0);
+	for (int i = 0; i < size; ++i){
+		s[i] = dis(gen);
+	}
+
+	// Create new sequence x_prime that enforces the prepartitioning from p
+	vector<signed long long> x_prime = vec_prime(x, s);
+	
+	int residue = kk(x_prime);
+
+	uniform_int_distribution<> rand_idx(0, size);
+
+	for (int i = 0; i < iterations; ++i){
+
+		int s_i = rand_idx(gen);
+		int s_j;
+		//make sure j != i
+		do {
+			s_j = rand_idx(gen);
+		} while (s[s_i] == s_j);
+
+		signed long long prev_ss1 = s[s_i];
+		s[s_i] = s_j;
+		
+		int temp_residue = kk(s);
+		if (temp_residue < residue){
+			residue = temp_residue;
+		}
+		else {
+			s[s_i] = prev_ss1;
+		}
+	}
+	return abs(residue);
 }
 
 int method_b_3(vector<signed long long>, int)
 {
+	// // Creating initial random solution
+	// vector<signed long long> p (size, 0);
+	// for (int i = 0; i < size; ++i){
+	// 	p[i] = dis(gen);
+	// }
 
+	// // Create new sequence x_prime that enforces the prepartitioning from p
+	// vector<signed long long> x_prime = vec_prime(x, p);
+	
+	// int residue = kk(x_prime);
+
+
+
+
+
+
+	// uniform_int_distribution<> rand_idx(0, x.size()-1);
+
+
+
+	// int residue_orig = residue;
+
+	// for (int i = 0; i < iterations; ++i){
+	// 	// for (int j = 0; j < x.size(); ++j){
+	// 	// 	printf("%i ", s[j]);
+	// 	// }
+	// 	// printf("\n");
+	// 	int s_i = rand_idx(gen);
+	// 	int s_j;
+	// 	//make sure j != i
+	// 	do {
+	// 		s_j = rand_idx(gen);
+	// 	} while (s_i == s_j);
+
+	// 	// following the algorithm-flip 1 or two elements of S
+	// 	int choosing = s_rand(gen);
+	// 	int temp_residue = residue - 2*s[s_i]*x[s_i];
+	// 	// 1/2 chance you change 2
+	// 	if (choosing == 0){
+	// 		temp_residue -= 2*s[s_j]*x[s_j];
+	// 	}
+	// 	// if its a smaller residue, change s
+	// 	if (abs(temp_residue)<abs(residue)){
+	// 		s[s_i] = -s[s_i];
+	// 		if (choosing == 0){
+	// 			s[s_j] = -s[s_j];
+	// 		}
+	// 		residue = temp_residue;
+	// 	}
+	// 	else{
+	// 		// "temperature" function
+	// 		float t_iter = 10000000000*pow(0.8, floor(i/300));
+	// 		float p = exp (-(abs(temp_residue)-abs(residue))/t_iter);
+	// 		bernoulli_distribution bern(p);
+	// 		bool anneal = bern(gen);
+	// 		if (anneal) {
+	// 			// printf("chosen \n");
+	// 			s[s_i] = -s[s_i];
+	// 			if (choosing == 0){
+	// 				s[s_j] = -s[s_j];
+	// 			}
+	// 			residue = temp_residue;
+	// 		}
+	// 	}
+
+	// 	if (abs(residue)<abs(residue_orig)){
+	// 		residue_orig = residue;
+	// 	}
+	// }
+	// return abs(residue_orig);
 }
